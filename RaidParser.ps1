456e214c425b6cbbc61920787f5e7a80817a0e9a
dhanrahl <dhanrahl@gmail.com>
2017-03-23 23:26:13 -0700

    Added missing CreationTime IF statement
    Checked to see if folder was created 2 days ago, but failed to check if
    (Get-Date) was a match to either CreationTime or CreationTime
    (Get-Date).AddDays(-2). If (Get-Date) is not either of the two given
    dates, the script quits.

--- RaidParser.ps1
+++ RaidParser.ps1
@@ -1,18 +1,20 @@
 ﻿#Location of Output Folder Path
 $FolderPath = "$env:USERPROFILE\Dropbox\Raid Parses\$OutputFolder"
 
-#Variables for Folder Creation
+#Variables for Folder Creation; Currently this creates folders with a "M-dd & M-dd" naming scheme for 2 raid days a week.
+$RaidDay = (Get-Date).ToString("M/dd/yyyy")
 $RaidDay1 = (Get-Date).ToString("M-dd")
-$RaidDay2 = (Get-Date).AddDays(2).ToString("M-dd")
+$RaidDay2 = (Get-Date).AddDays(-2).ToString("M-dd")
 $RaidDayFolder = "$env:USERPROFILE\Dropbox\Raid Parses\$RaidDay1 & $RaidDay2"
 $RaidFolder = Get-ChildItem $FolderPath | Sort-Object -Property CreationTime | Select-Object -Last 1
-$RaidFolderCreation1 = (Get-Date).AddDays(-2).ToString("M/dd/yyyy")
+$RaidFolderCreation = Get-ChildItem $FolderPath | Where-Object {$_.CreationTime.Date -Match $RaidDay}
+$RaidFolderCreation1 = (Get-Date).AddDays(-3).ToString("M/dd/yyyy")
 $RaidFolderCreation2 = Get-ChildItem $FolderPath | Where-Object {$_.CreationTime.Date -Match $RaidFolderCreation1} 
 
 #Variable for File Deletion
 $TestPatchRemove = Test-Path $FolderPath\*.evtc
 
-#Locations of Boss Logs
+#Locations of Boss EVTC Logs
 $ValeGuardian = "$env:USERPROFILE\Documents\arcdps\arcdps.cbtlogs\15438\*.evtc"
 $ValeGuardianCopy = Get-ChildItem "$env:USERPROFILE\Documents\arcdps\arcdps.cbtlogs\15438\*.evtc" | sort LastWriteTime | select -last 1
 $ValeGuardianLog = "$FolderPath\ValeGuardian.evtc"
@@ -57,14 +59,26 @@ $Deimos = "$env:USERPROFILE\Documents\arcdps\arcdps.cbtlogs\17154\*.evtc"
 $DeimosCopy = Get-ChildItem "$env:USERPROFILE\Documents\arcdps\arcdps.cbtlogs\17154\*.evtc" | sort LastWriteTime | select -last 1
 $DeimosLog = "$FolderPath\Deimos.evtc"
 
+#Checks location of Raid Folders to see if a folder exists for the two raid days. If no folder is present it will create a folder. Else Find and match the last known folder and set it as $OutputFolder for parsing.
 
-
-If ($RaidFolder = $RaidFolderCreation2 ) { 
+If ($RaidFolder = $RaidFolderCreation2) { #Determined today is RaidDay2, setting directory as $OutputFolder
     New-Variable -ErrorAction Ignore -Name "OutputFolder" -Value (Get-ChildItem -Directory -Path "$env:USERPROFILE\Dropbox\Raid Parses" | Sort-Object -Property CreationTime | select -last 1)
+    Write-Host "Successfuly set variable for OutputFolder"
 }
-Else { New-Item -Path "$env:USERPROFILE\Dropbox\Raid Parses" -Name "$RaidDay1 & $RaidDay2" -ItemType Directory }
+Else {  
+        If ($RaidFolder = $RaidDay) { #Checks to see if the raid folder that exists is from today, incase of new log file after running the script.
+            Write-Host "Failure: Today was not a raid day"
+            Pause
+            QUIT
+        }
+        Else { #Determined that there is no folder created for RaidDay1 & RaidDay2, proceed with folder creation and setting directory as $OutputFolder
+            New-Item -Path "$env:USERPROFILE\Dropbox\Raid Parses" -Name "$RaidDay1 & $RaidDay2" -ItemType Directory 
+            New-Variable -ErrorAction Ignore -Name "OutputFolder" -Value (Get-ChildItem -Directory -Path "$env:USERPROFILE\Dropbox\Raid Parses" | Sort-Object -Property CreationTime | select -last 1)
+            Write-Host "Successfully created new parse folder and set OutputFolder variable" 
+        }
+}
 
-#Parsing the logs
+#Parsing the logs. Checks the most recent EVTC log and compares to current date.
 
     if ( (Get-ChildItem $ValeGuardian | sort LastWriteTime | select -last 1).LastWriteTime -ge (get-date).Date ) {
         #If Get-ChildItem returned TRUE, run Get-ChildItem on directory to copy and rename
@@ -165,7 +179,7 @@ Else { New-Item -Path "$env:USERPROFILE\Dropbox\Raid Parses" -Name "$RaidDay1 & 
         Write-Host "Deimos was NOT created today."
     }  
 
-#Renaming Parses
+#Renaming log files after being parsed by RaidHeroes.exe
 
  If (Test-Path $FolderPath\ValeGuardian_vg.html) {
     Rename-Item $FolderPath\ValeGurdian_vg.html "Vale Guardian.html"
@@ -173,22 +187,22 @@ Else { New-Item -Path "$env:USERPROFILE\Dropbox\Raid Parses" -Name "$RaidDay1 & 
 Else {  }
 
  If (Test-Path $FolderPath\Gorseval_gorse.html) {
-    Rename-Item $FolderPath\Gorseval_gorse.html Gorseval.html
+    Rename-Item $FolderPath\Gorseval_gorse.html "Gorseval.html"
     }
 Else {  }
 
  If (Test-Path $FolderPath\Sabetha_sab.html) {
-    Rename-Item $FolderPath\Sabetha_sab.html Sabetha.html
+    Rename-Item $FolderPath\Sabetha_sab.html "Sabetha.html"
     }
 Else {  }
 
  If (Test-Path $FolderPath\Slothasor_sloth.html) {
-    Rename-Item $FolderPath\Slothasor_sloth.html Slothasor.html
+    Rename-Item $FolderPath\Slothasor_sloth.html "Slothasor.html"
     }
 Else {  }
 
  If (Test-Path $FolderPath\Matthias_matt.html) {
-    Rename-Item $FolderPath\Matthias_matt.html Matthias.html
+    Rename-Item $FolderPath\Matthias_matt.html "Matthias.html"
     }
 Else {  }
 
@@ -198,12 +212,12 @@ Else {  }
 Else {  }
 
  If (Test-Path $FolderPath\Xera_xera.html) {
-    Rename-Item $FolderPath\Xera_xera.html Xera.html
+    Rename-Item $FolderPath\Xera_xera.html "Xera.html"
     }
 Else {  }
 
  If (Test-Path $FolderPath\Cairn_cairn.html) {
-    Rename-Item $FolderPath\Cairn_cairn.html Cairn.html
+    Rename-Item $FolderPath\Cairn_cairn.html "Cairn.html"
     }
 Else {  }
 
@@ -213,12 +227,12 @@ Else {  }
 Else {  }
 
  If (Test-Path $FolderPath\Samarog_sam.html) {
-    Rename-Item $FolderPath\Samarog_sam.html Samarog.html
+    Rename-Item $FolderPath\Samarog_sam.html "Samarog.html"
     }
 Else {  }
 
  If (Test-Path $FolderPath\Deimos_dei.html) {
-    Rename-Item $FolderPath\Deimos_dei.html Deimos.html
+    Rename-Item $FolderPath\Deimos_dei.html "Deimos.html"
     }
 Else {  }
 
